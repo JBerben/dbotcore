24106fac33ec0a927fa39e97c533534451a840a3
DarkStorm <darkstorm@evilminecraft.net>
2013-03-07 18:34:57 -0500

    Renamed GameTypeSupportException -> UnsupportedGameTypeException

--- src/org/darkstorm/runescape/util/UnsupportedGameTypeException.java
+++ src/org/darkstorm/runescape/util/UnsupportedGameTypeException.java
@@ -2,30 +2,31 @@ package org.darkstorm.runescape.util;
 
 import org.darkstorm.runescape.GameType;
 
-public class GameTypeSupportException extends RuntimeException {
+@SuppressWarnings("serial")
+public class UnsupportedGameTypeException extends RuntimeException {
 	private final GameType type;
 
-	public GameTypeSupportException(GameType type) {
+	public UnsupportedGameTypeException(GameType type) {
 		this.type = type;
 	}
 
-	public GameTypeSupportException(String message, GameType type) {
+	public UnsupportedGameTypeException(String message, GameType type) {
 		super(message);
 		this.type = type;
 	}
 
-	public GameTypeSupportException(Throwable cause, GameType type) {
+	public UnsupportedGameTypeException(Throwable cause, GameType type) {
 		super(cause);
 		this.type = type;
 	}
 
-	public GameTypeSupportException(String message, Throwable cause,
+	public UnsupportedGameTypeException(String message, Throwable cause,
 			GameType type) {
 		super(message, cause);
 		this.type = type;
 	}
 
-	public GameTypeSupportException(String message, Throwable cause,
+	public UnsupportedGameTypeException(String message, Throwable cause,
 			boolean enableSuppression, boolean writableStackTrace, GameType type) {
 		super(message, cause, enableSuppression, writableStackTrace);
 		this.type = type;
