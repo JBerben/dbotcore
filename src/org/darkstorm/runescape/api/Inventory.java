f75a229c00797f5f49e45f6e092b4b7ce3eaf778
DarkStorm <darkstorm@evilminecraft.net>
2013-03-09 15:01:37 -0500

    Inventory.getCount(Filter<Item>)

--- src/org/darkstorm/runescape/api/Inventory.java
+++ src/org/darkstorm/runescape/api/Inventory.java
@@ -6,6 +6,8 @@ import org.darkstorm.runescape.api.wrapper.Item;
 public interface Inventory extends TypedUtility<Item> {
 	public boolean contains(Filter<Item> filter);
 
+	public int getCount(Filter<Item> filter);
+
 	public Item getItem(Filter<Item> filter);
 
 	public Item[] getItems(Filter<Item> filter);
