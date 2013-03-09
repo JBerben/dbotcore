0ce52a36db12949fd98eb6eadcdadb57a04d4f41
DarkStorm <darkstorm@evilminecraft.net>
2013-03-09 15:04:17 -0500

    Item should implement MouseTargetable even though it's Displayable.

--- src/org/darkstorm/runescape/api/wrapper/Item.java
+++ src/org/darkstorm/runescape/api/wrapper/Item.java
@@ -1,7 +1,9 @@
 package org.darkstorm.runescape.api.wrapper;
 
+import org.darkstorm.runescape.api.input.MouseTargetable;
 import org.darkstorm.runescape.api.util.*;
 
-public interface Item extends Identifiable, Displayable, Wrapper {
+public interface Item extends Identifiable, MouseTargetable, Displayable,
+		Wrapper {
 	public int getStackSize();
 }
